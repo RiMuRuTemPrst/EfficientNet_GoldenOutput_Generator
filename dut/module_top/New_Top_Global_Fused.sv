cd73aa2696ea848f5ed4d8db8c459eb5c086bca7
“RiMuRuTemPrst” <tquocanh2309@gmail.com>
2025-04-27 20:04:08 +0700

    Push New Top

--- dut/module_top/New_Top_Global_Fused.sv
+++ dut/module_top/New_Top_Global_Fused.sv
@@ -22,6 +22,13 @@ module New_Top_Global_Fused(
     logic [31:0] rd_addr_global;
     logic we_global;
 
+    //wire for Weight connect to PE_1x1 from BRAM
+    logic [31:0] Weight_0_n_state;
+    logic [31:0] Weight_1_n_state;
+    logic [31:0] Weight_2_n_state;
+    logic [31:0] Weight_3_n_state;
+    logic [31:0] addr_ram_next_rd;
+    logic [31:0] addr_w_n_state;
 
     logic [31:0] wr_addr_fused;
     logic [31:0] rd_addr_fused;
@@ -78,6 +85,27 @@ module New_Top_Global_Fused(
     // wire        wr_rd_req_Weight;
     // wire [31:0] wr_addr_Weight;
     // wire [31:0] wr_addr_IFM;
+
+    // wire data_mux and register for pipeline
+    wire [31:0] data_out_mux;
+    wire [7:0]  OFM_n_CONV_0;
+    wire [7:0]  OFM_n_CONV_1;
+    wire [7:0]  OFM_n_CONV_2;
+    wire [7:0]  OFM_n_CONV_3;
+    wire [7:0]  OFM_n_CONV_4;
+    wire [7:0]  OFM_n_CONV_5;
+    wire [7:0]  OFM_n_CONV_6;
+    wire [7:0]  OFM_n_CONV_7;
+    wire [7:0]  OFM_n_CONV_8;
+    wire [7:0]  OFM_n_CONV_9;
+    wire [7:0]  OFM_n_CONV_10;
+    wire [7:0]  OFM_n_CONV_11;
+    wire [7:0]  OFM_n_CONV_12;
+    wire [7:0]  OFM_n_CONV_13;
+    wire [7:0]  OFM_n_CONV_14;
+    wire [7:0]  OFM_n_CONV_15;
+    wire [7:0]  OFM_n_CONV_16;
+
 
     logic [31:0] base_addr =0;
     logic  [31:0] data_out_global_BRAM;
@@ -158,8 +186,8 @@ module New_Top_Global_Fused(
     .wr_rd_en(we_fused[1]),                               // Write enable
     .wr_addr(wr_addr_fused),            // Địa chỉ ghi
     .rd_addr(rd_addr_global),  // Địa chỉ đọc (địa chỉ byte → cần dịch)
-    .data_in(),               // Dữ liệu vào
-    .data_out()               // Dữ liệu ra
+    .data_in(data_out_global_BRAM),               // Dữ liệu vào
+    .data_out(Weight_1)               // Dữ liệu ra
     );
     BRAM_General #(
         .DATA_WIDTH_IN(128),
@@ -170,8 +198,8 @@ module New_Top_Global_Fused(
     .wr_rd_en(we_fused[2]),                               // Write enable
     .wr_addr(wr_addr_fused),            // Địa chỉ ghi
     .rd_addr(rd_addr_global),  // Địa chỉ đọc (địa chỉ byte → cần dịch)
-    .data_in(),               // Dữ liệu vào
-    .data_out()               // Dữ liệu ra
+    .data_in(data_out_global_BRAM),               // Dữ liệu vào
+    .data_out(Weight_2)               // Dữ liệu ra
     );
     BRAM_General #(
         .DATA_WIDTH_IN(128),
@@ -182,8 +210,8 @@ module New_Top_Global_Fused(
     .wr_rd_en(we_fused[3]),                               // Write enable
     .wr_addr(wr_addr_fused),            // Địa chỉ ghi
     .rd_addr(rd_addr_global),  // Địa chỉ đọc (địa chỉ byte → cần dịch)
-    .data_in(),               // Dữ liệu vào
-    .data_out()               // Dữ liệu ra
+    .data_in(data_out_global_BRAM),               // Dữ liệu vào
+    .data_out(Weight_3)               // Dữ liệu ra
     );
     BRAM_General #(
         .DATA_WIDTH_IN(128),
@@ -194,8 +222,8 @@ module New_Top_Global_Fused(
     .wr_rd_en(we_fused[4]),                               // Write enable
     .wr_addr(wr_addr_fused),            // Địa chỉ ghi
     .rd_addr(rd_addr_global),  // Địa chỉ đọc (địa chỉ byte → cần dịch)
-    .data_in(),               // Dữ liệu vào
-    .data_out()               // Dữ liệu ra
+    .data_in(data_out_global_BRAM),               // Dữ liệu vào
+    .data_out(Weight_4)               // Dữ liệu ra
     );
     BRAM_General #(
         .DATA_WIDTH_IN(128),
@@ -206,8 +234,8 @@ module New_Top_Global_Fused(
     .wr_rd_en(we_fused[5]),                               // Write enable
     .wr_addr(wr_addr_fused),            // Địa chỉ ghi
     .rd_addr(rd_addr_global),  // Địa chỉ đọc (địa chỉ byte → cần dịch)
-    .data_in(),               // Dữ liệu vào
-    .data_out()               // Dữ liệu ra
+    .data_in(data_out_global_BRAM),               // Dữ liệu vào
+    .data_out(Weight_5)               // Dữ liệu ra
     );
     BRAM_General #(
         .DATA_WIDTH_IN(128),
@@ -218,8 +246,8 @@ module New_Top_Global_Fused(
     .wr_rd_en(we_fused[6]),                               // Write enable
     .wr_addr(wr_addr_fused),            // Địa chỉ ghi
     .rd_addr(rd_addr_global),  // Địa chỉ đọc (địa chỉ byte → cần dịch)
-    .data_in(),               // Dữ liệu vào
-    .data_out()               // Dữ liệu ra
+    .data_in(data_out_global_BRAM),               // Dữ liệu vào
+    .data_out(Weight_6)               // Dữ liệu ra
     );
     BRAM_General #(
         .DATA_WIDTH_IN(128),
@@ -230,8 +258,8 @@ module New_Top_Global_Fused(
     .wr_rd_en(we_fused[7]),                               // Write enable
     .wr_addr(wr_addr_fused),            // Địa chỉ ghi
     .rd_addr(rd_addr_global),  // Địa chỉ đọc (địa chỉ byte → cần dịch)
-    .data_in(),               // Dữ liệu vào
-    .data_out()               // Dữ liệu ra
+    .data_in(data_out_global_BRAM),               // Dữ liệu vào
+    .data_out(Weight_7)               // Dữ liệu ra
     );
     BRAM_General #(
         .DATA_WIDTH_IN(128),
@@ -242,8 +270,8 @@ module New_Top_Global_Fused(
     .wr_rd_en(we_fused[8]),                               // Write enable
     .wr_addr(wr_addr_fused),            // Địa chỉ ghi
     .rd_addr(rd_addr_global),  // Địa chỉ đọc (địa chỉ byte → cần dịch)
-    .data_in(),               // Dữ liệu vào
-    .data_out()               // Dữ liệu ra
+    .data_in(data_out_global_BRAM),               // Dữ liệu vào
+    .data_out(Weight_8)               // Dữ liệu ra
     );
     BRAM_General #(
         .DATA_WIDTH_IN(128),
@@ -254,8 +282,8 @@ module New_Top_Global_Fused(
     .wr_rd_en(we_fused[9]),                               // Write enable
     .wr_addr(wr_addr_fused),            // Địa chỉ ghi
     .rd_addr(rd_addr_global),  // Địa chỉ đọc (địa chỉ byte → cần dịch)
-    .data_in(),               // Dữ liệu vào
-    .data_out()               // Dữ liệu ra
+    .data_in(data_out_global_BRAM),               // Dữ liệu vào
+    .data_out(Weight_9)               // Dữ liệu ra
     );
     BRAM_General #(
         .DATA_WIDTH_IN(128),
@@ -266,8 +294,8 @@ module New_Top_Global_Fused(
     .wr_rd_en(we_fused[10]),                               // Write enable
     .wr_addr(wr_addr_fused),            // Địa chỉ ghi
     .rd_addr(rd_addr_global),  // Địa chỉ đọc (địa chỉ byte → cần dịch)
-    .data_in(),               // Dữ liệu vào
-    .data_out()               // Dữ liệu ra
+    .data_in(data_out_global_BRAM),               // Dữ liệu vào
+    .data_out(Weight_10)               // Dữ liệu ra
     );
     BRAM_General #(
         .DATA_WIDTH_IN(128),
@@ -278,8 +306,8 @@ module New_Top_Global_Fused(
     .wr_rd_en(we_fused[11]),                               // Write enable
     .wr_addr(wr_addr_fused),            // Địa chỉ ghi
     .rd_addr(rd_addr_global),  // Địa chỉ đọc (địa chỉ byte → cần dịch)
-    .data_in(),               // Dữ liệu vào
-    .data_out()               // Dữ liệu ra
+    .data_in(data_out_global_BRAM),               // Dữ liệu vào
+    .data_out(Weight_11)               // Dữ liệu ra
     );
     BRAM_General #(
         .DATA_WIDTH_IN(128),
@@ -290,8 +318,8 @@ module New_Top_Global_Fused(
     .wr_rd_en(we_fused[12]),                               // Write enable
     .wr_addr(wr_addr_fused),            // Địa chỉ ghi
     .rd_addr(rd_addr_global),  // Địa chỉ đọc (địa chỉ byte → cần dịch)
-    .data_in(),               // Dữ liệu vào
-    .data_out()               // Dữ liệu ra
+    .data_in(data_out_global_BRAM),               // Dữ liệu vào
+    .data_out(Weight_12)               // Dữ liệu ra
     );
     BRAM_General #(
         .DATA_WIDTH_IN(128),
@@ -302,8 +330,8 @@ module New_Top_Global_Fused(
     .wr_rd_en(we_fused[13]),                               // Write enable
     .wr_addr(wr_addr_fused),            // Địa chỉ ghi
     .rd_addr(rd_addr_global),  // Địa chỉ đọc (địa chỉ byte → cần dịch)
-    .data_in(),               // Dữ liệu vào
-    .data_out()               // Dữ liệu ra
+    .data_in(data_out_global_BRAM),               // Dữ liệu vào
+    .data_out(Weight_13)               // Dữ liệu ra
     );
     BRAM_General #(
         .DATA_WIDTH_IN(128),
@@ -314,8 +342,8 @@ module New_Top_Global_Fused(
     .wr_rd_en(we_fused[14]),                               // Write enable
     .wr_addr(wr_addr_fused),            // Địa chỉ ghi
     .rd_addr(rd_addr_global),  // Địa chỉ đọc (địa chỉ byte → cần dịch)
-    .data_in(),               // Dữ liệu vào
-    .data_out()               // Dữ liệu ra
+    .data_in(data_out_global_BRAM),               // Dữ liệu vào
+    .data_out(Weight_14)               // Dữ liệu ra
     );
     BRAM_General #(
         .DATA_WIDTH_IN(128),
@@ -326,8 +354,8 @@ module New_Top_Global_Fused(
     .wr_rd_en(we_fused[15]),                               // Write enable
     .wr_addr(wr_addr_fused),            // Địa chỉ ghi
     .rd_addr(rd_addr_global),  // Địa chỉ đọc (địa chỉ byte → cần dịch)
-    .data_in(),               // Dữ liệu vào
-    .data_out()               // Dữ liệu ra
+    .data_in(data_out_global_BRAM),               // Dữ liệu vào
+    .data_out(Weight_15)               // Dữ liệu ra
     );
 
     BRAM_General #(
@@ -339,8 +367,8 @@ module New_Top_Global_Fused(
     .wr_rd_en(we_fused[16]),                               // Write enable
     .wr_addr(wr_addr_fused),            // Địa chỉ ghi
     .rd_addr(rd_addr_global),  // Địa chỉ đọc (địa chỉ byte → cần dịch)
-    .data_in(),               // Dữ liệu vào
-    .data_out()               // Dữ liệu ra
+    .data_in(data_out_global_BRAM),               // Dữ liệu vào
+    .data_out(Weight_16)               // Dữ liệu ra
     );
 
     BRAM_General #(
@@ -352,8 +380,8 @@ module New_Top_Global_Fused(
     .wr_rd_en(we_fused[17]),                               // Write enable
     .wr_addr(wr_addr_fused),            // Địa chỉ ghi
     .rd_addr(rd_addr_global),  // Địa chỉ đọc (địa chỉ byte → cần dịch)
-    .data_in(),               // Dữ liệu vào
-    .data_out()               // Dữ liệu ra
+    .data_in(data_out_global_BRAM),               // Dữ liệu vào
+    .data_out(Weight_17)               // Dữ liệu ra
     );
 
     BRAM_General #(
@@ -365,8 +393,8 @@ module New_Top_Global_Fused(
     .wr_rd_en(we_fused[18]),                               // Write enable
     .wr_addr(wr_addr_fused),            // Địa chỉ ghi
     .rd_addr(rd_addr_global),  // Địa chỉ đọc (địa chỉ byte → cần dịch)
-    .data_in(),               // Dữ liệu vào
-    .data_out()               // Dữ liệu ra
+    .data_in(data_out_global_BRAM),               // Dữ liệu vào
+    .data_out(Weight_18)               // Dữ liệu ra
     );
 
     BRAM_General #(
@@ -378,8 +406,8 @@ module New_Top_Global_Fused(
     .wr_rd_en(we_fused[19]),                               // Write enable
     .wr_addr(wr_addr_fused),            // Địa chỉ ghi
     .rd_addr(rd_addr_global),  // Địa chỉ đọc (địa chỉ byte → cần dịch)
-    .data_in(),               // Dữ liệu vào
-    .data_out()               // Dữ liệu ra
+    .data_in(data_out_global_BRAM),               // Dữ liệu vào
+    .data_out(Weight_19)               // Dữ liệu ra
     );
 
     address_generator addr_gen(
@@ -471,7 +499,174 @@ module New_Top_Global_Fused(
         .OFM_13(OFM_13),
         .OFM_14(OFM_14),
         .OFM_15(OFM_15)
-
     );
+    ReLU6 active0(
+        .OFM(OFM_0),
+        .OFM_active(OFM_active_0)
+    );
+    ReLU6 active1(
+        .OFM(OFM_1),
+        .OFM_active(OFM_active_1)
+    );
+    ReLU6 active2(
+        .OFM(OFM_2),
+        .OFM_active(OFM_active_2)
+    );
+    ReLU6 active3(
+        .OFM(OFM_3),
+        .OFM_active(OFM_active_3)
+    );
+    ReLU6 active4(
+        .OFM(OFM_4),
+        .OFM_active(OFM_active_4)
+    );
+    ReLU6 active5(
+        .OFM(OFM_5),
+        .OFM_active(OFM_active_5)
+    );
+    ReLU6 active6(
+        .OFM(OFM_6),
+        .OFM_active(OFM_active_6)
+    );
+    ReLU6 active7(
+        .OFM(OFM_7),
+        .OFM_active(OFM_active_7)
+    );
+    ReLU6 active8(
+        .OFM(OFM_8),
+        .OFM_active(OFM_active_8)
+    );
+    ReLU6 active9(
+        .OFM(OFM_9),
+        .OFM_active(OFM_active_9)
+    );
+    ReLU6 active10(
+        .OFM(OFM_10),
+        .OFM_active(OFM_active_10)
+    );
+    ReLU6 active11(
+        .OFM(OFM_11),
+        .OFM_active(OFM_active_11)
+    );
+    ReLU6 active12(
+        .OFM(OFM_12),
+        .OFM_active(OFM_active_12)
+    );
+    ReLU6 active13(
+        .OFM(OFM_13),
+        .OFM_active(OFM_active_13)
+    );
+    ReLU6 active14(
+        .OFM(OFM_14),
+        .OFM_active(OFM_active_14)
+    );
+    ReLU6 active15(
+        .OFM(OFM_15),
+        .OFM_active(OFM_active_15)
+    );
+
+    assign done_window_for_PE_cluster       =   {16{done_window_one_bit}};
+    assign finish_for_PE_cluster            =   (cal_start_ctl) && ( addr_IFM != 'b0 )   ? {16{finish_for_PE}} : 16'b0;
+    assign valid                            =   finish_for_PE_cluster;
+
+
+
+    Register_for_pipeline Reg_1(
+        .clk(clk),
+        .reset_n(reset_n),
+        .valid_data(done_window_for_PE_cluster),
+        .data_in_0(OFM_active_0),
+        .data_in_1(OFM_active_1),
+        .data_in_2(OFM_active_2),
+        .data_in_3(OFM_active_3),
+        .data_in_4(OFM_active_4),
+        .data_in_5(OFM_active_5),
+        .data_in_6(OFM_active_6),
+        .data_in_7(OFM_active_7),
+        .data_in_8(OFM_active_8),
+        .data_in_9(OFM_active_9),
+        .data_in_10(OFM_active_10),
+        .data_in_11(OFM_active_11),
+        .data_in_12(OFM_active_12),
+        .data_in_13(OFM_active_13),
+        .data_in_14(OFM_active_14),
+        .data_in_15(OFM_active_15),
+        .data_out_0(OFM_n_CONV_0),
+        .data_out_1(OFM_n_CONV_1),
+        .data_out_2(OFM_n_CONV_2),
+        .data_out_3(OFM_n_CONV_3),
+        .data_out_4(OFM_n_CONV_4),
+        .data_out_5(OFM_n_CONV_5),
+        .data_out_6(OFM_n_CONV_6),
+        .data_out_7(OFM_n_CONV_7),
+        .data_out_8(OFM_n_CONV_8),
+        .data_out_9(OFM_n_CONV_9),
+        .data_out_10(OFM_n_CONV_10),
+        .data_out_11(OFM_n_CONV_11),
+        .data_out_12(OFM_n_CONV_12),
+        .data_out_13(OFM_n_CONV_13),
+        .data_out_14(OFM_n_CONV_14),
+        .data_out_15(OFM_n_CONV_15)
+    );
+    wire [1:0]  control_mux_wire;
+    wire [31:0] addr_ram_next_wr_wire;
+    wire        wr_en_next_write;
+    wire        wr_data_valid;
+    Data_controller Data_controller_inst(
+        .clk(clk),
+        .rst_n(reset),
+        .OFM_data_out_valid(valid),
+        .control_mux(control_mux_wire),
+        .addr_ram_next_wr(addr_ram_next_wr_wire),
+        .wr_en_next(wr_en_next_write),
+        .wr_data_valid(wr_data_valid)
+    );
+
+    MUX_pipeline mux(
+        .control(control_mux_wire),
+
+        .data_in_0(OFM_n_CONV_0),
+        .data_in_1(OFM_n_CONV_1),
+        .data_in_2(OFM_n_CONV_2),
+        .data_in_3(OFM_n_CONV_3),
+        .data_in_4(OFM_n_CONV_4),
+        .data_in_5(OFM_n_CONV_5),
+        .data_in_6(OFM_n_CONV_6),
+        .data_in_7(OFM_n_CONV_7),
+        .data_in_8(OFM_n_CONV_8),
+        .data_in_9(OFM_n_CONV_9),
+        .data_in_10(OFM_n_CONV_10),
+        .data_in_11(OFM_n_CONV_11),
+        .data_in_12(OFM_n_CONV_12),
+        .data_in_13(OFM_n_CONV_13),
+        .data_in_14(OFM_n_CONV_14),
+        .data_in_15(OFM_n_CONV_15),
+
+        .data_out(data_out_mux)
+    );
+    BRAM_IFM BRAM_IFM_layer2(
+        .clk(clk),
+        .rd_addr(addr_ram_next_rd),
+        .wr_addr(addr_ram_next_wr_wire),
+        .wr_rd_en(wr_en_next_write),
+        .data_in(data_out_mux),
+        .data_out(out_BRAM_CONV)
+    );
+    wire [3:0] PE_reset_n_state_wire;
+    PE_cluster_1x1 PE_cluster_1x1(
+        .clk(clk),
+        .reset_n(reset),
+        .PE_reset(PE_reset_n_state_wire),
+        .Weight_0(Weight_0_n_state),
+        .Weight_1(Weight_1_n_state),
+        .Weight_2(Weight_2_n_state),
+        .Weight_3(Weight_3_n_state),
+        .IFM(out_BRAM_CONV),
+        .OFM_0(OFM_0_n_state),
+        .OFM_1(OFM_1_n_state),
+        .OFM_2(OFM_2_n_state),
+        .OFM_3(OFM_3_n_state)
+    );
+
 
 endmodule
